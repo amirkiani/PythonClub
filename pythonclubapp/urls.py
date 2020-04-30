b14c01fa30ae11c9ffe159a666d4e9b9b74259ef
amirkiani <53153060+amirkiani@users.noreply.github.com>
2020-04-29 18:08:50 -0700

    updated urls.py
    updated urls with additional path for getproducts

--- pythonclubapp/urls.py
+++ pythonclubapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns=[
     path('', views.index, name='index'),
     path('gettypes/', views.gettypes, name='types'),
+    path('getproducts/', views.getproducts, name='products')
 ]
